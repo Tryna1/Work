b45b2b07d4649a6fb63dace08de42d0904a854b6
Tryna1 <1761726520@qq.com>
2020-12-22 15:45:30 +0800

    第五次作业

---
+++ src/Work5/_9_5.java
@@ -0,0 +1,20 @@
+package Work5;
+import java.util.GregorianCalendar;
+
+public class _9_5 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+		int m =  gregorianCalendar.get(GregorianCalendar.MONTH);
+		m += 1;
+		System.out.println(gregorianCalendar.get(GregorianCalendar.YEAR) + "å¹?" + m + 
+				"æœ?" + gregorianCalendar.get(GregorianCalendar.DAY_OF_MONTH) + "æ—?");
+		gregorianCalendar.setTimeInMillis(1234567898765L);
+		m =  gregorianCalendar.get(GregorianCalendar.MONTH);
+		m += 1;
+		System.out.println(gregorianCalendar.get(GregorianCalendar.YEAR) + "å¹?" + m + 
+				"æœ?" + gregorianCalendar.get(GregorianCalendar.DAY_OF_MONTH) + "æ—?");
+	}
+
+}
